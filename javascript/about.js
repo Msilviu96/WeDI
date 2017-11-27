3aa8ae79b6cfca424a4adf5b11ce42a5057222c5
Silviu Constantin MUNTEANU <smunteanu@ias.bitdefender.biz>
2017-11-28 00:38:19 +0200

    added about page

---
+++ javascript/about.js
@@ -0,0 +1,18 @@
+window.onload = function(){
+	var silviu = document.getElementById("silviu");
+	var vlad = document.getElementById("vlad");
+	var git = document.getElementById("git");
+
+
+	silviu.onclick = function(){
+		window.open('https://www.facebook.com/munteanu.silviu.79');
+	};
+	
+	vlad.onclick = function(){
+		window.open('https://www.facebook.com/vlad.ventaniuc');
+	};
+	
+	git.onclick = function(){
+		window.open("https://github.com/Msilviu96/WeDI");
+	};
+};
