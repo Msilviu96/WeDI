8017de34723e06d82a242f699066ddbd5cd9a37a
vlad <vladimir.ventaniuc@yahoo.com>
2017-11-28 12:12:10 +0200

    a

--- javascript/content.js
+++ javascript/content.js
@@ -1,9 +1,4 @@
-/* Listen for messages */
-chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
-	/* If the received message has the expected format... */
-	if (msg.text && (msg.text == "report_back")) {
-        /* Call the specified callback, passing 
-           the web-pages DOM content as argument */
-		sendResponse(document.getElementById("mybutton").innerHTML);
-	}
-});
+var iFrame = document.createElement("iframe");
+iFrame.src = chrome.extension.getURL("../html/popup.html");
+
+document.body.insertBefore(iFrame, document.body.firstChild);
